88f76cc041a920502a5a11e82ce16fc3ab2789a9
ashiq73 <ashiq.buet73@gmail.com>
2023-07-10 20:53:45 +0600

    update login  page v1.2

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ function func() {
   var password = document.getElementById("password").value;
 
   if (username == "admin" && password == "ug@2023") {
-    location.href = "https://ashiqee.github.io/ug-admission-2023/#";
+    location.href = "https://ashiqee.github.io/ug-admission-2023/home.html";
     alert("login succesfully");
   } else {
     alert("login fail !");
